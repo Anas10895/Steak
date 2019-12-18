7331f33e1a396205444f93755d7024fc2c369dc4
AnasShar <anas10895@gmail.com>
2019-12-18 09:25:43 +0300

    new item

--- coffee/src/functionAuth/functionAuth.js
+++ coffee/src/functionAuth/functionAuth.js
@@ -28,14 +28,14 @@ export const login = (user)=>{
 }
 export const add = newItem => {
     return axios
-      .put("http://localhost:2551/api/v1/users/5df8c917778c9a4e637c9663/orders/neworder", newItem)
+      .put("http://localhost:2551/api/v1/stores/5df8cc4d778c9a4e637c9664/items/newitem", newItem)
       .then(res => console.log(res))
       .catch(err => console.log(err));
   };
 let id ="5df8c917778c9a4e637c9663"
   export const Order = newOrder => {
     axios
-     .post("/api/v1/users/5df8c917778c9a4e637c9663/orders/neworder", newOrder)
+     .post("http://localhost:2551/api/v1/orders/neworder", newOrder)
      .then(res => console.log(res))
      .catch(err => console.log(err));
  };
